371a3816bdb348c1d21fa61183c2632153587dea
notSagyo <notsagyo@gmail.com>
2022-05-03 19:01:17 -0300

    Desafio02

---
+++ dist/desafio02.js
@@ -0,0 +1,84 @@
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const fs = require('fs');
+class Container {
+  name = 'output';
+  constructor(name) {
+    this.name = name;
+  }
+  save(obj) {
+    const exists = fs.existsSync(this.name);
+    let file;
+    let id;
+    try {
+      file = exists ? fs.readFileSync(this.name, 'utf-8') : '';
+      file = file && file.length > 0 ? file : '[]';
+      const parsedFile = JSON.parse(file);
+      parsedFile.push(obj);
+      file = JSON.stringify(parsedFile, null, 2);
+      id = parsedFile.length - 1;
+      fs.writeFileSync(this.name, file);
+    }
+    catch (err) {
+      console.error(err);
+      return null;
+    }
+    return id;
+  }
+  getbyId(id) {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      const result = parsedFile[id];
+      return result;
+    }
+    catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+  getAll() {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      return parsedFile;
+    }
+    catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+  deleteById(id) {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      parsedFile.splice(id, 1);
+      const newFile = JSON.stringify(parsedFile, null, 2);
+      fs.writeFileSync(this.name, newFile);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+  async deleteAll() {
+    try {
+      fs.writeFileSync(this.name, '');
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+}
+// Test Methods ==============================================================//
+const cats = new Container('cats.json');
+cats.deleteAll();
+// Fill the file with some data
+cats.save({ breed: 'Siamese', age: 3 });
+cats.save({ breed: 'Sphynx', age: 1 });
+cats.save({ breed: 'Egyptian', age: 4 });
+cats.save({ breed: 'Persian', age: 7 });
+cats.save({ breed: 'Savannah', age: 2 });
+console.log('Cat ID[0]:', cats.getbyId(0));
+console.log('All cats:', cats.getAll());
+cats.deleteById(0);
+console.log('After delete ID[0]:', cats.getAll());
---
+++ src/desafio02.ts
@@ -0,0 +1,90 @@
+import * as fs from 'fs';
+
+class Container {
+  name = 'output';
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  save(obj: Record<string, unknown>) {
+    const exists = fs.existsSync(this.name);
+    let file: string;
+    let id: number;
+
+    try {
+      file = exists ? fs.readFileSync(this.name, 'utf-8') : '';
+      file = file && file.length > 0 ? file : '[]';
+
+      const parsedFile = JSON.parse(file);
+      parsedFile.push(obj);
+
+      file = JSON.stringify(parsedFile, null, 2);
+      id = parsedFile.length - 1;
+
+      fs.writeFileSync(this.name, file);
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+
+    return id;
+  }
+
+  getbyId(id: number) {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      const result = parsedFile[id];
+      return result;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  getAll() {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      return parsedFile;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+
+  deleteById(id: number) {
+    try {
+      const file = fs.readFileSync(this.name, 'utf-8');
+      const parsedFile = JSON.parse(file);
+      parsedFile.splice(id, 1);
+      const newFile = JSON.stringify(parsedFile, null, 2);
+      fs.writeFileSync(this.name, newFile);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async deleteAll() {
+    try { fs.writeFileSync(this.name, ''); }
+    catch (err) { console.error(err); }
+  }
+}
+
+// Test Methods ==============================================================//
+const cats = new Container('cats.json');
+cats.deleteAll();
+
+// Fill the file with some data
+cats.save({ breed: 'Siamese', age: 3 });
+cats.save({ breed: 'Sphynx', age: 1 });
+cats.save({ breed: 'Egyptian', age: 4 });
+cats.save({ breed: 'Persian', age: 7 });
+cats.save({ breed: 'Savannah', age: 2 });
+
+console.log('Cat ID[0]:', cats.getbyId(0));
+console.log('All cats:', cats.getAll());
+
+cats.deleteById(0);
+console.log('After delete ID[0]:', cats.getAll());
