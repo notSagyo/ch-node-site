7d4879d7fdd80f12e625ebf0f88432145d543559
notSagyo <notsagyo@gmail.com>
2022-05-19 18:59:41 -0300

    Build TS

--- dist/app.js
+++ dist/app.js
@@ -5,10 +5,11 @@ const products_api_1 = require("./products-api");
 // INIT ======================================================================//
 const PORT = 8080;
 const app = express();
-const productsApi = new products_api_1.ProductsAPI();
-app.use(express.json());
+const productsApi = new products_api_1.default();
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', productsApi.apiRouter);
+app.use(express.json());
+app.use('/api', productsApi.router);
+app.use(express.static('public'));
 // listen ====================================================================//
 app.listen(PORT, () => {
     console.log(`Server started at http://localhost:${PORT}/`);
---
+++ dist/product.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Product = void 0;
+const placeholder = 'https://via.placeholder.com/256';
+class Product {
+    // Manual ID will be ignored when saving in the container
+    id;
+    name;
+    price;
+    thumbnail;
+    constructor(name, price, thumbnail, id) {
+        this.name = name;
+        this.price = price;
+        this.thumbnail = thumbnail || placeholder;
+        this.id = id || 0;
+    }
+    static parseProduct(obj) {
+        const isValidPrice = !isNaN(Number(obj?.price)) && Number(obj?.price) > 0;
+        const name = typeof obj?.name === 'string' && obj?.name ? obj.name : null;
+        const price = isValidPrice ? Number(obj.price) : null;
+        const thumbnail = typeof obj?.thumbnail === 'string' ? obj.thumbnail : placeholder;
+        const id = typeof obj?.id === 'number' ? obj.id : 0;
+        if (name != null && price != null)
+            return new Product(name, price, thumbnail, id);
+        return null;
+    }
+}
+exports.Product = Product;
